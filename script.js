f9d40f057ab1916c2abc6591103468785530b0b7
Florian290394 <florian-feissel@web.de>
2025-09-14 11:43:27 +0200

    Buchung Mai und Dezember

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ const bookingEvents = [
   { title: 'Belegt', start: '2025-08-28', end: '2025-09-01', display: 'background', color: '#e74c3c' },
   { title: 'Belegt', start: '2025-06-13', end: '2025-06-16', display: 'background', color: '#e74c3c' },
   { title: 'Belegt', start: '2026-05-01', end: '2026-05-04', display: 'background', color: '#e74c3c' },
+  { title: 'Belegt', start: '2026-05-22', end: '2026-05-27', display: 'background', color: '#e74c3c' },
+  { title: 'Belegt', start: '2025-12-06', end: '2025-12-08', display: 'background', color: '#e74c3c' },
 ];
 function renderGallery() {
   const gallery = document.querySelector('.gallery');
