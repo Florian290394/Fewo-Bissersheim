e2d016a3933845d3711dd703c32442305407af9c
Florian290394 <florian-feissel@web.de>
2026-07-26 12:43:05 +0200

    Anpassung Buchung Fischer

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const bookingEvents = [
   { title: 'Belegt', start: '2026-10-24', end: '2026-10-30', display: 'background', color: '#e74c3c' }, // 2E Fechner
 
   //2027
-  { title: 'Belegt', start: '2027-04-30', end: '2027-05-02', display: 'background', color: '#e74c3c' }, // 3E Fischer
+  { title: 'Belegt', start: '2027-04-30', end: '2027-05-03', display: 'background', color: '#e74c3c' }, // 3E Fischer
   { title: 'Belegt', start: '2027-06-04', end: '2027-06-08', display: 'background', color: '#e74c3c' }, // 2E Pich
   { title: 'Belegt', start: '2027-08-11', end: '2027-08-16', display: 'background', color: '#e74c3c' }, // 2E Markou
 ];
